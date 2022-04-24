16e0905a603ceeb07b143b9cf3d52a8ee277b824
gstyanti <101311934+gstyanti@users.noreply.github.com>
2022-04-24 23:58:09 +0800

    Add files via upload

---
+++ Dosen.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UTS_no_4
+{
+    public class Dosen
+    {
+        //properties
+        public string id { get; set; }
+        public string Nama_Dosen { get; set; }
+        public string NIK { get; set; }
+        public string Gender { get; set; }
+        public string Course { get; set; }
+
+        //method 
+        public void dosen()
+        {
+            Console.WriteLine("Id: {0}", id);
+            Console.WriteLine("Nama Dosen Pengampu: {0}", Nama_Dosen);
+            Console.WriteLine("NIK: {0}", NIK);
+            Console.WriteLine("Jenis Kelamin: {0}", Gender);
+            Console.WriteLine("Pengampu Mata Kuliah {0}", Course);
+        }
+        
+        public void attDosen()
+        {
+           
+            string Nama = Console.ReadLine();
+            string NIK = Console.ReadLine();
+            
+        }
+
+        public void addCourse()
+        {
+            string course = Console.ReadLine();
+        }
+    }
+
+    
+    
+
+}
---
+++ Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UTS_no_4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // membuat objek dari class dosen
+            Dosen pengampu = new Dosen ();
+
+            Console.Write("Nama Dosen Pengampu: \n");
+            pengampu.Nama_Dosen = Console.ReadLine();
+
+            Console.Write("NIK: \n");
+            pengampu.NIK = Console.ReadLine();
+
+            Console.Write("Mengampu Mata Kuliah: \n");
+            pengampu.Course = Console.ReadLine();
+
+            // mengeset nilai 
+            pengampu.id = "2468\n";
+            pengampu.Nama_Dosen = "Kamarudin, M.Kom\n";
+            pengampu.NIK = "985643\n";
+            pengampu.Gender = "Pria\n";
+            pengampu.Course = "Pemrograman\n";
+
+            //memanggil method 
+            pengampu.dosen();
+            pengampu.attDosen();
+            pengampu.addCourse();
+
+            Console.ReadKey();
+        }
+    }
+}
